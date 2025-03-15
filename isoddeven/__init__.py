d89ae28fa075052158fb2910207115bccc0311a7
Nilay Sarma <79686507+nilaysarma@users.noreply.github.com>
2025-03-15 09:06:26 +0530

    Update v0.2.2

--- isoddeven/__init__.py
+++ isoddeven/__init__.py
@@ -3,7 +3,7 @@ isoddeven - A fun Python package to find out if a number is odd or even vouched 
 """
 
 from .isoddeven import is_odd
-from .isoddeven import is_odd
+from .isoddeven import is_even
 from .isoddeven import state
 from .extras import is_up
 from .extras import about
