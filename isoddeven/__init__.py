3e4a17aaa55034888f6ac4960610042fa64432aa
Nilay Sarma <79686507+nilaysarma@users.noreply.github.com>
2025-03-16 08:50:40 +0530

    Update v0.4.0

--- isoddeven/__init__.py
+++ isoddeven/__init__.py
@@ -1,5 +1,8 @@
-"""isoddeven - A Python package to check if a number is odd or even."""
+"""isoddeven
+=====
+A Python package to check if a number is odd or even.
+"""
 
-from .isoddeven import is_odd
-from .isoddeven import is_even
+from .isoddeven import isodd
+from .isoddeven import iseven
 from .isoddeven import state
--- isoddeven/isoddeven.py
+++ isoddeven/isoddeven.py
@@ -1,20 +1,103 @@
-def is_odd(n: int) -> bool:
+"""isoddeven - A Python package to check if a number is odd or even."""
+
+def isodd(n: int) -> bool:
+    """Return true if the number is odd.
+
+    Check if a number is odd. It returns true if the number is odd and false if the number is even.
+
+    Parameters
+    ----------
+    n : int
+        The number to check.
+
+    Returns
+    -------
+    bool
+        True if `n` is odd, False otherwise.
+
+    Raises
+    ------
+    TypeError
+        If `n` is not an integer.
+
+    Examples
+    --------
+    >>> isoddeven.isodd(3)
+    True
+    >>> isoddeven.isodd(4)
+    False
+    """
+
     try:
         return n % 2 != 0
     except TypeError:
         print("Expected an integer value.")
 
-def is_even(n: int) -> bool:
+def iseven(n: int) -> bool:
+    """Return true if the number is even.
+
+    Check if a number is even. It returns true if the number is even and false if the number is odd.
+
+    Parameters
+    ----------
+    n : int
+        The number to check.
+
+    Returns
+    -------
+    bool
+        True if `n` is even, False otherwise.
+
+    Raises
+    ------
+    TypeError
+        If `n` is not an integer.
+
+    Examples
+    --------
+    >>> isoddeven.iseven(2)
+    True
+    >>> isoddeven.iseven(5)
+    False
+    """
+
     try:
         return n % 2 == 0
     except TypeError:
         print("Expected an integer value.")
 
 def state(n: int) -> str:
+    """Check whether the number is odd or even.
+    
+    Return "odd" if the number is odd and "even" if the number is even.
+
+    Parameters
+    ----------
+    n : int
+        The number to check.
+
+    Returns
+    -------
+    str
+        "odd" if `n` is odd, "even" if `n` is even.
+
+    Raises
+    ------
+    TypeError
+        If `n` is not an integer.
+
+    Examples
+    --------
+    >>> isoddeven.state(2)
+    even
+    >>> isoddeven.state(3)
+    odd
+    """
+
     try:
-        if is_odd(n):
+        if isodd(n):
             return "odd"
-        elif is_even(n):
+        elif iseven(n):
             return "even"
     except TypeError:
         print("Expected an integer value.")
